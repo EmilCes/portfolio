b0b8b5c7e7a2eeae1ec75f709fe2ffa5561a0fd9
Cesar Emiliano Lezama Lopez <cesar@MacBook-Air-de-Cesar.local>
2021-09-17 17:52:13 -0500

    Proyects

--- Portfolio/JS/main.js
+++ Portfolio/JS/main.js
@@ -1,6 +1,6 @@
 console.log("hola");
 ScrollReveal().reveal('.widget', { interval: 250 });
 ScrollReveal().reveal('.title', {delay:400, origin:top, opacity:0, easing: 'cubic-bezier(1, 0, 0, 0.8)'})
-ScrollReveal().reveal('.info', {delay:350, origin:top, opacity:0, easing: 'cubic-bezier(1, 0, 0, 0.8)'})
+ScrollReveal().reveal('.info', {delay:250, origin:top, opacity:0, easing: 'cubic-bezier(1, 0, 0, 0.8)'})
 ScrollReveal().reveal('.info-title', {delay:600, origin:top, opacity:0, easing: 'cubic-bezier(1, 0, 0, 0.8)'})
 
